1ea9451176e5a729c0d215a31acce994c772c23d
julialuzzib <juliabaldissera06@gmail.com>
2025-09-26 15:40:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//exemplo 01
+const botao1 = document.getElementById("botao1");
+const paragrafo1 = document.getElementById("paragrafo1");
+
+botao1.addEventListener("click", function () {
+    paragrafo1.textContent = "Parágrafo alterado ao clicar no botão!";
+});
+
+//exemplo 02
+
+
+
+//exemplo 03
